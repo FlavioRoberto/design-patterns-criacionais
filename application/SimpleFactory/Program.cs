68f5eabef30e3d9d20159381807b5d4a6b2e7857
flaviorobertoteixeira <flavio.r.teixeira@outlook.com>
2023-03-11 13:47:12 -0300

    feat: created Program to test pizza factory

---
+++ application/SimpleFactory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace factory.SimpleFactory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pizza pizza;
+            Console.WriteLine("Select your pizza (P)epperoni or (M)ozzarella");
+            var selectedPizza = Console.ReadLine()?.ToUpper() ?? string.Empty;
+            try
+            {
+                pizza = PizzaFactory.Create(selectedPizza);
+                pizza.Prepare();
+                pizza.Bake(60);
+                pizza.Pack();
+                Console.WriteLine("Pizza delivered with success");
+            }
+            catch (ApplicationException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
