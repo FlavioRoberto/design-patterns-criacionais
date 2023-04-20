4fcbdde38299912578a2b004020bbeeffd74fde5
flaviorobertoteixeira <flavio.r.teixeira@outlook.com>
2023-04-20 19:57:32 -0300

    feat: creating singleton project

---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+using static System.Console;
+
+WriteLine("Trying to create the first singleton instance");
+var instance = SingletonExample.Instance;
+
+WriteLine("Trying to create the second singleton instance");
+var instance2 = SingletonExample.Instance;
+
+if (instance == instance2)
+    WriteLine("Has one instance");
+else
+    WriteLine("Have two instances");
+
+ReadLine();
+
---
+++ Singleton/Singleton/SingletonExample.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+namespace Singleton
+{
+    public class SingletonExample
+    {
+        private static SingletonExample instance;
+        private int numberOfInstance = 0;
+
+        private SingletonExample()
+        {
+            WriteLine("Incresing number of instance");
+            numberOfInstance++;
+            WriteLine($"number of instance: {numberOfInstance}");
+        }
+
+        public static SingletonExample Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    WriteLine("Creating firts instance");
+                    instance = new SingletonExample();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
+
